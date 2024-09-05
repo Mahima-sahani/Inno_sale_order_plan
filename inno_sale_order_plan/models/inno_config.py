45d24687316f87425607d94c06937d11373d34c0
Mahima-sahani <mahima.sahani@innoage.in>
2024-09-05 11:54:38 +0530

    Delete inno_sale_order_plan directory

--- inno_sale_order_plan/models/inno_config.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class InnoConfig(models.Model):
-    _inherit = 'inno.config'
-
-    dyeing_journal_id = fields.Many2one(comodel_name='account.journal', string='Dyeing Journal')
--- inno_sale_order_plan/models/product.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.template'
-
-    is_verified = fields.Boolean(string='Verified?')
